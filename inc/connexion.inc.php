32a11e38e1b1cfdd5385f04fa716c9b0f219c249
Loïc Bourdain <loic.bourdain@gmail.com>
2019-06-05 22:37:29 +0200

    Commit final

--- inc/connexion.inc.php
+++ inc/connexion.inc.php
@@ -5,11 +5,11 @@ $pdo_options[PDO::MYSQL_ATTR_INIT_COMMAND]= 'SET NAMES utf8';
 $serveur = "localhost";
 $loginserveur = "root";
 $mdpserveur = "";
-/* locale */ $serveur = "localhost"; $bddname = "cnamcp09_nfa083cf1_2018";
-/* locale */ $loginserveur = "root"; $mdpserveur = "";
+/* locale */// $serveur = "localhost"; $bddname = "cnamcp09_nfa083cf1_2018";
+/* locale */// $loginserveur = "root"; $mdpserveur = "";
 
-/* online */// $serveur = "localhost"; $bddname = "cnamcp09_nfa083cf1_2018";
-/* online */// $loginserveur = "root"; $mdpserveur = "";
+/* online */ $serveur = "sql204.epizy.com"; $bddname = "epiz_24007113_cnamcp09_nfa083cf1_2018";
+/* online */ $loginserveur = "epiz_24007113"; $mdpserveur = "QU51qJyVNGX7";
 
 
 $con = new PDO('mysql:host='.$serveur.';dbname='.$bddname,$loginserveur,$mdpserveur,$pdo_options);
